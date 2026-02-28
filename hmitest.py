c155af2e0b81c8fc2c0193812bbcdd4cff2e4ff0
sdavi187 <sdavi187@kennesaw.edu>
2026-02-27 19:13:47 -0500

    Create hmitest.py
    Initial file creation

---
+++ hmitest.py
@@ -0,0 +1,34 @@
+
+def check_string (string_to_check, big_size = 99, min_size = 5):
+
+    #big_size = 99
+
+    print (string_to_check)
+    print (big_size)
+    print (min_size)
+
+    if string_to_check.isalpha():
+        return ("The string is all letters")
+    elif string_to_check.isdigit():
+        
+       
+        string_to_check = int(string_to_check)
+
+        if string_to_check > big_size:
+            print ("THe number is big")
+        else:
+            print("The number is small")
+
+        return ("The string is all numbers")
+
+    else:
+        return ("The string is mixed")
+    
+while (True):
+    input_string = input ("Enter a string for evaluation: ")
+
+    if input_string != "":
+        print (check_string(input_string, 400, 6))
+        break
+    else:
+        print ("You must enter something.")
